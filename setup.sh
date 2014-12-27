71c66341d16f452cdc4ab0edf132c4b7a985d0cf
Tim 'mithro' Ansell <mithro@mithis.com>
2014-12-27 21:19:28 +1030

    Trying to get the setup script working.

--- setup.sh
+++ setup.sh
@@ -3,8 +3,15 @@
 set +x
 set +e
 
+# Get the latest chroot details
+wget http://travis-trusty.timvideos.us/index.txt -O chroot-details.txt
+CHROOT_TAR_INFO=$(cat chroot-details.txt | grep tar.bz2 | tail -n 1)
+CHROOT_TAR_FILE=$(echo $CHROOT_TAR_INFO | sed -e's/[^ ]* //')
+CHROOT_TAR_MD5=$(echo $CHROOT_TAR_INFO | sed -e's/ .*//')
+
 # Download and extra the trusty chroot
-wget XXXXXX
+TRUSTY_TAR=/tmp/ubuntu-trusty-chroot.tar.bz2
+wget http://travis-trusty.timvideos.us/$CHROOT_TAR_FILE -O $TRUSTY_TAR
 sudo mkdir /trusty
 sudo tar -xvf $TRUSTY_TAR /trusty
 
@@ -12,6 +19,3 @@ sudo tar -xvf $TRUSTY_TAR /trusty
 for mountpoint in /dev /sys /proc /home; do
   sudo mount -o bind $mountpoint /trusty$mountpoint
 done
-
-# Run the real set up script
-$PWD/.travis-run-trusty.sh $PWD/.travis-setup.sh
