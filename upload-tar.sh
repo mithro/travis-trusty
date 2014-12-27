8258f76ab6f4d9c1a634496d67a92dc5a7f760c3
Tim 'mithro' Ansell <mithro@mithis.com>
2014-12-27 21:30:32 +1100

    Updating upload-tar.sh

--- upload-tar.sh
+++ upload-tar.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-for FILE in *.tar.*; do
-  turbolift \
-    -u $RACKSPACE_USER \
-    -a $RACKSPACE_API \
-    --os-rax-auth iad \
-    upload \
-    -c travis-trusty \
-    -s $FILE
-done
+TB_COMMAND="turbolift -u $RACKSPACE_USER -a $RACKSPACE_API --os-rax-auth iad upload -c travis-trusty"
+
+# Upload the tar bz
+$TB_COMMAND -s . --sync --pattern-match ".*\.tar\..*"
+
+# Generate an index file
+md5sum *.tar.* | sort -k2 > index.txt
+$TB_COMMAND -s index.txt
