67ca3a693cc809914b34c1f3415374534c11fe21
Tim 'mithro' Ansell <mithro@mithis.com>
2014-12-28 13:53:23 +0000

    Fixes.

--- docker-base/run.py
+++ docker-base/run.py
@@ -4,4 +4,4 @@ import os
 print "argv", repr(sys.argv)
 cmd = ["sudo", "-E", "chroot", "/trusty", "su", "-p", os.environ['USER'], "-c", " ".join('"%s"' % a for a in sys.argv[1:])]
 print " cmd", repr(cmd)
-os.execlp(cmd)
+os.execlp(*cmd)
