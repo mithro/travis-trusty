c8150f6430ece7351d67ca3918fd93c5eb043a41
Tim 'mithro' Ansell <mithro@mithis.com>
2014-12-28 14:07:35 +0000

    Actually run stage2.

--- docker-base/run.py
+++ docker-base/run.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 import sys
 import os
+stage2 = ["/run-stage2.py", os.getcwd()] + sys.argv[1:]
 if "DEBUG_TRUSTY" in os.environ:
     print "argv", repr(sys.argv)
-cmd = ["sudo", "-E", "chroot", "/trusty", "su", "-p", os.environ['USER'], "-c", " ".join('"%s"' % a for a in sys.argv[1:])]
+    print "stg2", repr(stage2)
+cmd = ["sudo", "-E", "chroot", "/trusty", "su", "-p", os.environ['USER'], "-c", " ".join('"%s"' % a for a in stage2)]
 if "DEBUG_TRUSTY" in os.environ:
     print " cmd", repr(cmd)
 os.execlp(*cmd)
